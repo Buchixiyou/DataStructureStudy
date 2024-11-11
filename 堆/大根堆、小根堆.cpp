115fbb58127cc30445a3b34ee97786e6df7fb6f5
不吃西柚 <1394448968@qq.com>
2024-11-11 20:19:02 +0800

    堆

--- 堆/大根堆、小根堆.cpp
+++ 堆/大根堆、小根堆.cpp
@@ -18,7 +18,7 @@ struct HeapStruct {
 typedef Heap MaxHeap; //最大堆
 typedef Heap MinHeap; //最小堆
 
-Heap Creat(int MaxSize) {
+Heap CreatM(int MaxSize) {
     MaxHeap H = (MaxHeap)malloc(sizeof(struct HeapStruct));
     H->Elyments = (ElymentType*)malloc((MaxSize+1)*sizeof(ElymentType));
     H->Size = 0;
@@ -78,7 +78,7 @@ ElymentType DeleteMax(MaxHeap H) {
 
 
 //建造最大堆
-void PercDown(MaxHeap H,int p) {
+void PercDownM(MaxHeap H,int p) {
     int parent,child;
     ElymentType X;
 
@@ -95,12 +95,37 @@ void PercDown(MaxHeap H,int p) {
     H->Elyments[parent]=X;
 }
 
-void BuildHeap(MaxHeap H) {
+void BuildHeapM(MaxHeap H) {
     for(int i = H->Size/2;i>0;i--) {
-        PercDown(H,i);
+        PercDownM(H,i);
     }
 }
 
 
 //小根堆操作相反即可
+
+//建最小堆
+void PercDownN(MinHeap H,int p) {
+    int parent,child;
+    ElymentType X = H->Elyments[0];
+
+    for(parent =p;parent*2<H->Size;parent=child) {
+        child = parent*2;
+        if((child!=H->Size)&&(H->Elyments[child]>H->Elyments[child+1]))
+            child++;
+        if(H->Elyments[child]>=X) break;
+
+        else {
+            H->Elyments[parent]=H->Elyments[child];
+        }
+    }
+    H->Elyments[parent]=X;
+}
+
+void BuildHeapN(MinHeap H) {
+    for(int i = H->Size/2;i>0;i--) {
+        PercDownN(H,i);
+    }
+}
+
 
